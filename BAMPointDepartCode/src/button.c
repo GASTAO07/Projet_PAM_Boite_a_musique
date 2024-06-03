de3318d3c6e6242caf5ca682be33d370b197c322
Rogério <gastaorogerio53@gmail.com>
2024-06-03 21:44:31 +0200

    correction_dossier

---
+++ BAMPointDepartCode/src/button.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include "sys/cm4.h"
+#include "sys/devices.h"
+#include "sys/init.h"
+#include "sys/clock.h"
+#include "button.h"
+
+void button_init()
+{
+
+}
+
+void button_irq_init ()
+{
+
+}
+
+uint32_t button_poll()
+{
+
+}
+
+
---
+++ BAMPointDepartCode/src/buzzer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include "sys/cm4.h"
+#include "sys/devices.h"
+#include "sys/init.h"
+#include "sys/clock.h"
+#include "buzzer.h"
+
+void buzzer_pwm_init (uint32_t TIM2_freq)
+{
+
+}
+
+void toggle_buzzer()
+{
+
+}
